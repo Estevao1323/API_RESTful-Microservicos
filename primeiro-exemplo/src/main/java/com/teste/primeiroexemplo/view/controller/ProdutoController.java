25de78226fbfaf35435b04682ff02845cf5ceba8
Estevao1323 <estevaocmontes@gmail.com>
2024-01-02 18:22:40 -0300

    Ajustando o DTO no ProdutoController

--- primeiro-exemplo/src/main/java/com/teste/primeiroexemplo/view/controller/ProdutoController.java
+++ primeiro-exemplo/src/main/java/com/teste/primeiroexemplo/view/controller/ProdutoController.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +18,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.teste.primeiroexemplo.model.Produto;
+
 import com.teste.primeiroexemplo.services.ProdutoService;
 import com.teste.primeiroexemplo.shared.ProdutoDTO;
+import com.teste.primeiroexemplo.view.model.ProdutoRequest;
 import com.teste.primeiroexemplo.view.model.ProdutoResponse;
 
 @RestController
@@ -60,19 +61,32 @@ public class ProdutoController {
     }
 
     @PostMapping
-    public Produto adicionar(@RequestBody Produto produto){
-        return produtoService.adicionar(produto);
+    public ResponseEntity<ProdutoResponse> adicionar(@RequestBody ProdutoRequest produtoRequest){
+        ModelMapper mapper = new ModelMapper();
+
+        ProdutoDTO produtoDto = mapper.map(produtoRequest, ProdutoDTO.class);
+
+        produtoDto = produtoService.adicionar(produtoDto);
+
+        return new ResponseEntity<>(mapper.map(produtoDto, ProdutoResponse.class), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public String deletar(@PathVariable Integer id){
+    public ResponseEntity<?> deletar(@PathVariable Integer id){
         produtoService.deletar(id);
-            return "Produto com id:" + id + " foi deletado com sucesso!";
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 //@Requestbody: converte o corpo da requisição em um produto
 @PutMapping("/{id}")
-    public Produto atualizar(@RequestBody Produto produto, @PathVariable Integer id){
-        return produtoService.atualizar(id, produto);
+    public ResponseEntity<ProdutoResponse> atualizar(@RequestBody ProdutoRequest produtoRequest, @PathVariable Integer id){
+       ModelMapper mapper = new ModelMapper();
+       ProdutoDTO produtoDto = mapper.map(produtoRequest, ProdutoDTO.class);
+
+       produtoDto = produtoService.atualizar(id, produtoDto);
+
+        return new ResponseEntity<>(
+            mapper.map(produtoDto, ProdutoResponse.class),
+            HttpStatus.OK);
     }
 }
